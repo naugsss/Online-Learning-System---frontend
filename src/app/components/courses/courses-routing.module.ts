013da0b82e3c1d2ea42a27c53ea4fa7184acc28e
Aaryan Naugaraiya <naugaria.ar.6@gmail.com>
2023-12-20 15:31:15 +0530

    minor bug fixes

--- src/app/components/courses/courses-routing.module.ts
+++ src/app/components/courses/courses-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { CoursesComponent } from './courses.component';
 import { CoursePreviewComponent } from './course-preview/course-preview.component';
+import { AuthGuard } from '../login/auth.guard';
 
 const routes: Routes = [
   {
     path: 'courses',
     component: CoursesComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: ':courseName',
